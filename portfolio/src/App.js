c648f673610b48861037effddd9754b0a4a5de57
anusha-c18 <anushac2001@gmail.com>
2023-05-08 21:10:30 +0530

    Added dummy navigation bar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React, { useState } from "react";
 import Welcome from "./components/Preloader/Welcome";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   const [preloader, setPreloader] = useState("true");
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      {!preloader ? <Navigation></Navigation> : null}
       {preloader ? (
         <Welcome visibility={updatePreloaderVisibility}></Welcome>
       ) : null}
